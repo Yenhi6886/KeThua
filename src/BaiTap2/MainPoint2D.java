c0f6bfccbb96ac5496efd7307a782c17341b3228
Yenhi6886 <nguyenyenhi220904@gmail.com>
2025-05-15 15:02:09 +0700

    add Bai 2

---
+++ src/BaiTap2/MainPoint2D.java
@@ -0,0 +1,11 @@
+package BaiTap2;
+
+public class MainPoint2D {
+    public static void main(String[] args) {
+        Point2D p2d = new Point2D();
+        System.out.println(p2d);
+
+        p2d= new Point2D(5,3);
+        System.out.println(p2d);
+    }
+}
---
+++ src/BaiTap2/MainPoint3D.java
@@ -0,0 +1,11 @@
+package BaiTap2;
+
+public class MainPoint3D {
+    public static void main(String[] args) {
+       Point3D m = new Point3D();
+        System.out.println(m);
+
+        m = new Point3D(2,5,6);
+        System.out.println(m);
+    }
+}
